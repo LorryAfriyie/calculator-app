d3663f37ccac9fe2358791d6afc1738e103c8794
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-10-31 03:24:45 +0200

    Tristate toggle switch
    Creating tristate toggle switch and encapsulating the code within its own component.

--- src/components/SwitchToggle.tsx
+++ src/components/SwitchToggle.tsx
@@ -2,29 +2,20 @@ export function ToggleSwitch() {
   return (
     <div className="toggle-switch">
       <div className={"toggle-switch__switch"}>
-        <label htmlFor="one">1</label>
-        <input
-          type="radio"
-          className={"toggle-switch__input"}
-          id={"one"}
-          name={"theme"}
-        />
+        <label htmlFor="one" className="toggle-switch__theme-switcher">
+          <input type="radio" id="one" name={"toggle"} />
+          <span className="checkmark"></span>
+        </label>
 
-        <label htmlFor="two">2</label>
-        <input
-          type="radio"
-          className={"toggle-switch__input"}
-          id={"two"}
-          name={"theme"}
-        />
+        <label htmlFor="two" className="toggle-switch__theme-switcher">
+          <input type="radio" id="two" name={"toggle"} />
+          <span className="checkmark"></span>
+        </label>
 
-        <label htmlFor="three">3</label>
-        <input
-          type="radio"
-          className={"toggle-switch__input"}
-          id={"three"}
-          name={"theme"}
-        />
+        <label htmlFor="three" className="toggle-switch__theme-switcher">
+          <input type="radio" id="three" name={"toggle"} />
+          <span className="checkmark"></span>
+        </label>
       </div>
     </div>
   );
