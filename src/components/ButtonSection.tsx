09c3b3d46c81550b56e1779fd52315e4f3c05d58
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-12-25 15:24:44 +0200

    Code refactor and button styling
    Removing commented code that will not be used and removing DRY code in scss button file.

--- src/components/ButtonSection.tsx
+++ src/components/ButtonSection.tsx
@@ -58,7 +58,7 @@ export function ButtonSection() {
     { value: 2, text: "2", method: buttonValue },
     { value: 3, text: "3", method: buttonValue },
     { value: "-", text: "-", method: signHandler },
-    { value: ".", text: ".", method: buttonValue },
+    { value: ".", text: ".", method: commaHandler },
     { value: 0, text: "0", method: buttonValue },
     { value: "/", text: "/", method: signHandler },
     { value: "*", text: "*", method: signHandler },
@@ -75,46 +75,6 @@ export function ButtonSection() {
           );
         })}
       </div>
-
-      {/* <div className="first-row">
-        <Button onClick={buttonValue} value={7} text={"7"} />
-
-        <Button onClick={buttonValue} value={8} text={"8"} />
-
-        <Button onClick={buttonValue} value={9} text={"9"} />
-
-        <Button onClick={deleteValue} text={"del"} />
-      </div>
-
-      <div className="second-row">
-        <Button onClick={buttonValue} value={4} text={"4"} />
-
-        <Button onClick={buttonValue} value={5} text={"5"} />
-
-        <Button onClick={buttonValue} value={6} text={"6"} />
-
-        <Button onClick={signHandler} value={"+"} text={"+"} />
-      </div>
-
-      <div className="third-row">
-        <Button onClick={buttonValue} value={1} text={"1"} />
-
-        <Button onClick={buttonValue} value={2} text={"2"} />
-
-        <Button onClick={buttonValue} value={3} text={"3"} />
-
-        <Button onClick={signHandler} value={"-"} text={"-"} />
-      </div>
-
-      <div className="fourth-row">
-        <Button onClick={commaHandler} text={"."} value={"."} />
-
-        <Button onClick={buttonValue} value={0} text={"0"} />
-
-        <Button onClick={signHandler} value={"/"} text={"/"} />
-
-        <Button onClick={signHandler} value={"*"} text={"x"} />
-      </div> */}
 
       <div className="fifth-row">
         <Button onClick={resetCalculator} text={"Reset"} />
